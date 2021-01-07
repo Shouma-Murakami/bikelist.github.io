484897e7b3f0eb12df819b16573693218297d9c8
Shouma-Murakami <76508378+Shouma-Murakami@users.noreply.github.com>
2021-01-08 00:45:20 +0900

    Add files via upload

---
+++ js/scriptA.js
@@ -0,0 +1,14 @@
+
+$(function(){
+  $('.list-item').on('dblclick', function() {
+    $(this).css('color','black');
+  });
+  $('.reset-btn').on('click',function(){
+    $('.list-item').css('color','black');
+  });
+  $('.list-item').longpress(function(e){
+    $('.list-item').css('color','black');
+  },function(e){
+    $(this).css('color','red');
+  })
+});
